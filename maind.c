f5a4c8c9ffa3ae1c1a09f4723493c60974156ea5
Andy Neff <andy@visionsystemsinc.com>
2022-03-04 18:49:50 -0500

    Clean up
    Signed-off-by: Andy Neff <andy@visionsystemsinc.com>

--- maind.c
+++ maind.c
@@ -8,17 +8,14 @@ int main()
   void (*my_funcb)();
   void (*my_func2)(int);
 
-  void *handle_main = dlopen("./libmain.so", RTLD_LAZY);
-  *(void**)(&my_funca) = dlsym(handle_main, "foo_a");
-  *(void**)(&my_funcb) = dlsym(handle_main, "foo_b");
-  my_funca();
-  my_funcb();
-  dlclose(handle_main);
+  void *handle_main;
+  void *handlea;
+  void *handleb;
 
-  void *handlea = dlopen("./libad.so", RTLD_LAZY||RTLD_LOCAL);
+  handlea = dlopen("./libad.so", RTLD_LAZY);
   *(void**)(&my_funca) = dlsym(handlea, "foo_a");
 
-  void *handleb = dlopen("./libbd.so", RTLD_LAZY||RTLD_LOCAL);
+  handleb = dlopen("./libbd.so", RTLD_LAZY);
   *(void**)(&my_funcb) = dlsym(handleb, "foo_b");
 
   my_funca(); // crashes here?
@@ -27,5 +24,25 @@ int main()
   dlclose(handlea);
   dlclose(handleb);
 
+  // Broken examples
+
+  handle_main = dlopen("./libmain.so", RTLD_LAZY);
+  *(void**)(&my_funca) = dlsym(handle_main, "foo_a");
+  *(void**)(&my_funcb) = dlsym(handle_main, "foo_b");
+  my_funca();
+  my_funcb();
+  // dlclose(handle_main); // this will unbreak a and b below
+
+  handlea = dlopen("./libad.so", RTLD_LAZY);
+  *(void**)(&my_funca) = dlsym(handlea, "foo_a");
+
+  handleb = dlopen("./libbd.so", RTLD_LAZY);
+  *(void**)(&my_funcb) = dlsym(handleb, "foo_b");
+
+  my_funca(); // crashes here?
+  my_funcb();
+
+  dlclose(handlea);
+  dlclose(handleb);
   dlclose(handle_main);
 }
